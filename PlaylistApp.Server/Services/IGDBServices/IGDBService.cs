cb6bbab64593ffc611614ae481cd9b820fed5ca8
ethan.enger <engeethan@gmail.com>
2024-09-17 19:18:18 -0600

    IGDB service to add some games to the database

--- PlaylistApp.Server/Services/IGDBServices/IGDBService.cs
+++ PlaylistApp.Server/Services/IGDBServices/IGDBService.cs
@@ -13,7 +13,7 @@ public class IGDBService : IIGDBService
     private IGDBClient igdbClient;
     public IGDBService()
     {
-        
+
     }
 
     public IGDBService(IDbContextFactory<PlaylistDbContext> contextFactory, IGDBClient igdbClient)
@@ -25,13 +25,17 @@ public class IGDBService : IIGDBService
     public async Task<JsonArray> GetGamesFromIGDB(string query)
     {
         var values = await igdbClient.QueryWithResponseAsync<IGDB.Models.Game>(IGDBClient.Endpoints.Games, query);
+
+        //transform the data into values we can actually use
         var array = JsonArray.Parse(values.StringContent ?? "");
+
         return array!.AsArray();
     }
 
     public async Task PostGamesToDatabase(JsonArray games)
     {
         using var context = contextFactory.CreateDbContext();
+
         foreach (var game in games)
         {
             context.Games.Add(ParseGameIntoLocalGame(game!.AsObject()));
@@ -48,17 +52,76 @@ public class IGDBService : IIGDBService
         using (var document = JsonDocument.Parse(jsonString))
         {
             var root = document.RootElement;
-            game.Title = root.GetProperty("name").GetString() ?? "";
-            game.AgeRating = ((AgeRatingCategory)root.GetProperty("age_ratings")
-                .EnumerateArray()
-                .Where(x => x.GetProperty("category").GetInt32() == 1)
-                .FirstOrDefault()
-                .GetProperty("rating")
-                .GetInt32()).ToString();
-            game.CoverUrl = root.GetProperty("cover").GetProperty("url").ToString().Replace("t_thumb", "t_cover_big");
-            game.Description = root.GetProperty("summary").GetString() ?? string.Empty;
-            game.PublishDate = UnixTimeToDateTime(root.GetProperty("release_dates").EnumerateArray().Min(x => x.GetProperty("date").GetUInt32()));
-            game.IdgbId = root.GetProperty("id").GetInt32();
+            JsonElement foundProperty;
+
+            //try get id
+            if(root.TryGetProperty("id", out foundProperty))
+            {
+                game.IdgbId = foundProperty.GetInt32();
+            }
+            else
+            {
+                throw new Exception("No IGDB Id found, needed for creation");
+            }
+
+            //Try Get title
+            if (root.TryGetProperty("name", out foundProperty))
+            {
+                game.Title = foundProperty.GetString() ?? throw new Exception("No name found, needed for creation");
+            }
+
+            //Try Get esrb category
+            if (root.TryGetProperty("age_ratings", out foundProperty))
+            {
+                JsonElement foundCategory;
+                foreach(var item in foundProperty.EnumerateArray())
+                {
+                    if(item.TryGetProperty("category", out foundCategory))
+                    {
+                        //1 is the category of ESRB
+                        if(foundCategory.GetUInt32() != 1)
+                        {
+                            continue;
+                        }
+                        var ratingNum = item.GetProperty("rating").GetUInt32();
+                        game.AgeRating = ((AgeRatingCategory)ratingNum).ToString();
+                    }
+                }
+
+                //If no age rating was found replace with default value
+            }
+                if(game.AgeRating is null)
+                {
+                    game.AgeRating = "NaN";
+                }
+
+
+            //Try Get Cover Url
+            if(root.TryGetProperty("cover", out foundProperty))
+            {
+                JsonElement foundUrl;
+                if(foundProperty.TryGetProperty("url", out foundUrl))
+                {
+                    game.CoverUrl = (foundUrl.GetString() ?? "").Replace("t_thumb", "t_cover_big");
+                }
+            }
+
+            //Try Get Description
+            if(root.TryGetProperty("summary", out foundProperty))
+            {
+                game.Description = foundProperty.GetString();
+            }   
+            else
+            {
+                game.Description = "This game does not have a description yet. You can contact us if you would like to add a description you made.";
+            }
+
+            //Try get publish date
+            if(root.TryGetProperty("first_release_date", out foundProperty))
+            {
+                game.PublishDate = UnixTimeToDateTime(foundProperty.GetUInt32());
+            }
+
         }
         return game;
     }
