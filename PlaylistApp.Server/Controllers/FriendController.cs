68fbdbea9b7d11841db0214d22046b437d57cc8f
ParkerS00 <parker.swenson1@students.snow.edu>
2024-09-24 20:43:03 -0600

    Friend Controller Implemented and Tested

---
+++ PlaylistApp.Server/Controllers/FriendController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlaylistApp.Server.DTOs;
+using PlaylistApp.Server.Requests.AddRequests;
+using PlaylistApp.Server.Requests.UpdateRequests;
+using PlaylistApp.Server.Services.FriendServices;
+
+namespace PlaylistApp.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FriendController : Controller
+{
+    private readonly IFriendService friendService;
+
+    public FriendController(IFriendService friendService)
+    {
+        this.friendService = friendService; 
+    }
+
+    [HttpPatch("acceptfriend")]
+    public async Task<bool> AcceptFriend(AcceptFriendRequest request)
+    {
+        return await friendService.AcceptFriend(request);
+    }
+
+    [HttpPost("addfriend")]
+    public async Task<bool> AddFriend(AddFriendRequest request)
+    {
+        return await friendService.AddFriend(request);
+    }
+
+    [HttpGet("getallfriendsbybaseid")]
+    public async Task<List<UserDTO>> GetAllFriendsByBaseId(Guid baseId)
+    {
+        return await friendService.GetAllFriendsByBaseId(baseId);
+    }
+
+    [HttpGet("getfriendbyid")]
+    public async Task<FriendDTO> GetFriendById(int friendId)
+    {
+        return await friendService.GetFriendById(friendId);
+    }
+
+    [HttpDelete("removefriend")]
+    public async Task<bool> RemoveFriend(int friendId)
+    {
+        return await friendService.RemoveFriend(friendId);
+    }
+}
