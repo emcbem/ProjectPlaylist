be430c5db3b9f9802bdea14a778739407d3b8548
ParkerS00 <parker.swenson1@students.snow.edu>
2024-09-30 18:27:40 -0600

    Platform Game Controller Implemented and Tested

---
+++ PlaylistApp.Server/Controllers/PlatformGameController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlaylistApp.Server.DTOs;
+using PlaylistApp.Server.Requests.GetRequests;
+using PlaylistApp.Server.Services.PlatformGameServices;
+
+namespace PlaylistApp.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlatformGameController : Controller
+{
+    private readonly IPlatformGameService platformGameService;
+
+    public PlatformGameController(IPlatformGameService platformGameService)
+    {
+        this.platformGameService = platformGameService;
+    }
+
+    [HttpPost("GetAllPlatformGames")]
+    public async Task<List<PlatformGameDTO>> GetAllPlatformGame(PlatformGameRequest request)
+    {
+        return await platformGameService.GetAllPlatformGames(request);
+    }
+}
