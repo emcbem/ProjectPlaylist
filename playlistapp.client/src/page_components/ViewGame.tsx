d1457bca235045b0078f8cc27235a6924730e674
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-10-01 21:01:04 -0600

    Epic shading styles YAY! (づ｡◕‿‿◕｡)づ

--- playlistapp.client/src/page_components/ViewGame.tsx
+++ playlistapp.client/src/page_components/ViewGame.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GameContext } from "../contexts/GameContext";
 import { Game, GameContextInterface } from "../@types/game";
+import "./game.css";
 
 const ViewGame = () => {
   const { games } = React.useContext(GameContext) as GameContextInterface;
   const { gameId } = useParams<{ gameId: string }>();
   const [game, setgame] = useState<Game>();
-
 
   useEffect(() => {
     console.log(games);
@@ -18,21 +18,19 @@ const ViewGame = () => {
     <>
       <div className="min-h-screen bg-white dark:bg-black">
         <div className="m-6">
-          <h1 className="text-white">{game?.title}</h1>
-          <p className="text-white">Game Id: {gameId}</p>
-          <div className="relative flex justify-center top-0 h-screen mt-[-10rem]">
+          <div className="relative flex justify-center top-0 h-screen mt-[-6rem]">
             <img
               src={game?.coverUrl.replace(/t_cover_big/g, "t_1080p")}
-              className="object-cover object-top z-0 w-full h-full pt-2 px-64"
+              className={
+                "object-cover object-top z-0 w-full h-full 2xl:px-64 xl:px-56 lg:px-44 md:px-32 sm:px-20 px-0 image-gradient"
+              }
             />
-            <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-black"></div>
           </div>
-
-          {/* Darker gradient overlay */}
-          <div className="absolute inset-0 pointer-events-none bg-[radial-gradient(ellipse_at_top,transparent_10%,black_80%)] opacity-90"></div>
+          <h1 className="dark:text-white text-black mt-[-60rem] text-8xl font-extrabold mx-28 underline"> {game?.title}</h1>
+          <h1 className="dark:text-yellow-500 text-4xl font-extrabold mx-28 mt-8">Ratings: 9.25/10 - Leave your own.</h1>
         </div>
       </div>
-      <button className="relative z-20 text-white">Add to Library</button>
+      <button className="relative z-20 dark:text-white text-black">Add to Library</button>
     </>
   );
 };
