681e766513a36b3b01823ec6217c216c277771ea
Dusty Shaw <shustydaw@gmail.com>
2024-10-08 17:25:02 -0600

    Added masonry cards on account

---
+++ playlistapp.client/src/individual_components/MasonryCardsUsrGame.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { PS5Icon } from "../assets/SmallPlatforms/psLogo";
+import { SteamIcon } from "../assets/SmallPlatforms/steamLogo";
+import { XboxIcon } from "../assets/SmallPlatforms/xboxLogo";
+import { UserGame } from "@/@types/usergame";
+
+interface props {
+  title: string;
+  games: UserGame[];
+}
+
+const options: Intl.DateTimeFormatOptions = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+const MasonryCards: React.FC<props> = ({ games }) => {
+  const [expand, setExpand] = useState<boolean>(false);
+  const cards = games.map((game) => {
+    const randomHeight = Math.floor(Math.random() * (300 - 200 + 1) + 200);
+    return (
+  
+        <div className="rounded-lg overflow-hidden dark:text-white text-black">
+          <img
+            src={game.platformGame.game.coverUrl}
+            className={`w-full sm:h-[${randomHeight}px] h-[${
+              randomHeight / 2
+            }px] object-cover`}
+          />
+          <div className="dark:bg-[#252A2C] bg-[#D9D9D9] hidden">
+            <div className="flex flex-row p-2">
+              <PS5Icon height={20} width={20} darkColor="white" color="black" />
+              <XboxIcon height={20} width={20} darkColor="white" color="black" />
+              <SteamIcon height={20} width={20} darkColor="white" color="black" />
+            </div>
+            <div className="sm:text-2xl text-base px-2 pt-2 font-extrabold">
+              {game.platformGame.game.title}
+            </div>
+            <div className="sm:text-base text-xs px-2 pt-2 ">
+              Rating: {game.platformGame.game.ageRating}
+            </div>
+            <div className="sm:text-base text-xs px-2 py-2 ">
+              Release:{" "}
+              {game.platformGame.game.publishDate
+                ? new Date(game.platformGame.game.publishDate).toLocaleDateString(
+                    undefined,
+                    options
+                  )
+                : ""}
+            </div>
+          </div>
+        </div>
+ 
+    );
+  });
+
+  return (
+    <div className="flex-grow w-full dark:text-white text-black">
+      <div
+        className={`relative lg:mx-20 md:mx-15 sm:mx-10 mx-5 ${
+          expand
+            ? " "
+            : "overflow-hidden lg:h-[50rem] md:h-[45rem] sm:h-[40rem] h-[25rem]"
+        }  `}
+      >
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{
+            300: 2,
+            600: 3,
+            900: 4,
+            1100: 5,
+            1600: 6,
+            1800: 8,
+          }}
+        >
+          <Masonry gutter="10px">{cards}</Masonry>
+        </ResponsiveMasonry>
+        {!expand && (
+          <div className="absolute bottom-0 left-0 bg-gradient-to-b from-transparent dark:to-black dark:via-black/80 to-white via-white/80 from-20% via-40% to-100% w-full h-96 z-10"></div>
+        )}
+      </div>
+      <div
+        className=" flex lg:text-2xl md:text-xl sm:text-lg text-base font-bold lg:mx-20 md:mx-15 sm:mx-10 mx-5 mb-5 justify-end cursor-pointer"
+        onClick={() => setExpand(!expand)}
+      >
+        {expand ? "See Less." : "See more."}
+      </div>
+    </div>
+  );
+};
+
+export default MasonryCards;
--- playlistapp.client/src/page_components/Account.tsx
+++ playlistapp.client/src/page_components/Account.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HorizontalRule from '../individual_components/HorizontalRule';
 import { UserGameContext } from '../contexts/UserGameContext';
 import { UserGameContextInterface } from '../@types/usergame';
+import MasonryCardsUsrGame from '@/individual_components/MasonryCardsUsrGame';
 
 const Account = () => {
     const { userGames } = React.useContext(UserGameContext) as UserGameContextInterface;
@@ -12,7 +13,7 @@ const Account = () => {
     return (
         isAuthenticated &&
         user && (
-            <div className="min-h-screen bg-white">
+            <div className="min-h-screen bg-white dark:bg-black dark:text-white">
                 <div className="m-8">
                     <div className="text-4xl">Welcome Back, {user.nickname}</div>
                     <HorizontalRule />
@@ -22,15 +23,17 @@ const Account = () => {
                     </div>
                     <p className="mt-8 text-6xl">Your Collection</p>
                     <HorizontalRule />
-                    {userGames &&
+
+                    <MasonryCardsUsrGame title={"Your Collection"} games={userGames} />
+                    {/* {userGames &&
                         userGames.map((ug, key) =>
                             <div key={key}>
-                                {/* <img src={ug.platformGame.game.coverUrl} width={100}/>
-                                <p>Platform Game Id: {ug.platformGame.game.coverUrl}</p> */}
+                                <img src={ug.platformGame.game.coverUrl} width={100}/>
+                                <p>Platform Game Id: {ug.platformGame.game.coverUrl}</p> 
                                 <p>Time Played: {ug.timePlayed}</p>
                             </div>
                         )
-                    }
+                    } */}
                 </div>
             </div>
         )
