dd31d09064a4dab4277bb5200f741e3b0964294f
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-10-22 20:32:27 -0600

    Have the reviews pulling from the database, still has bug with refreshing

--- playlistapp.client/src/QueryKeys/GameReviewKeys.ts
+++ playlistapp.client/src/QueryKeys/GameReviewKeys.ts
@@ -1,9 +1,10 @@
 const GameReviewKeys = {
-    AddGameReview: ["GameReview", "AddGameReview"] as const,
-    GetGameReviewById: ["GameReview", "GetGameReviewById"] as const,
-    GetAllGameReviewsByGame: ["GameReview", "GetAllGameReviewsByGame"] as const,
-    UpdateGameReview: ["GameReview", "UpdateGameReview"] as const,
-    DeleteGameReview: ["GameReview", "DeleteGameReview"] as const,
-}
+  AddGameReview: ["GameReview", "AddGameReview"] as const,
+  GetGameReviewById: ["GameReview", "GetGameReviewById"] as const,
+  GetAllGameReviewsByGame: (gameId: number) =>
+    ["GameReview", "GetAllGameReviewsByGame", `Game/${gameId}`] as const,
+  UpdateGameReview: ["GameReview", "UpdateGameReview"] as const,
+  DeleteGameReview: ["GameReview", "DeleteGameReview"] as const,
+};
 
-export default GameReviewKeys
+export default GameReviewKeys;
