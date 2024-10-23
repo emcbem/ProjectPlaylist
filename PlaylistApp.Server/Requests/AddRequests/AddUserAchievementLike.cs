cd59b76a463d0ebd70ab2e5cf0425cade90175fc
ParkerS00 <parker.swenson1@students.snow.edu>
2024-10-22 18:51:44 -0600

    Can add a user achievement like in the front end

--- PlaylistApp.Server/Requests/AddRequests/AddUserAchievementLike.cs
+++ PlaylistApp.Server/Requests/AddRequests/AddUserAchievementLike.cs
@@ -4,7 +4,7 @@ namespace PlaylistApp.Server.Requests.AddRequests;
 
 public class AddUserAchievementLike
 {
-    public int AchievementId { get; set; }
+    public int UserAchievementId { get; set; }
     public Guid UserId { get; set; }
     public bool IsLike { get; set; }
 }
--- playlistapp.client/src/ApiServices/UserAchievementLikeService.ts
+++ playlistapp.client/src/ApiServices/UserAchievementLikeService.ts
@@ -10,7 +10,7 @@ export const UserAchievementLikeService = {
       throw new Error("Add user achievement like request must be provided");
     }
     try {
-      const response = await axios.post<number>(
+      const response = await axios.post<boolean>(
         `${
           import.meta.env.VITE_URL
         }/UserAchievementLike/adduserachievementlike`,
