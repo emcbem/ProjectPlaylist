bd00a147d7e6f35587721d86b06e8adff8b9775a
Dusty Shaw <shustydaw@gmail.com>
2024-10-28 20:43:29 -0600

    Lists can be deleted

--- playlistapp.client/src/page_components/MyLibrary/MyLibraryListView.tsx
+++ playlistapp.client/src/page_components/MyLibrary/MyLibraryListView.tsx
@@ -12,7 +12,7 @@ const MyLibraryListView: FC<MyLibraryGridViewProps> = ({ games }) => {
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" className="px-6 py-3">
+                            <th scope="col" className="px-6 py-3  xl:block lg:block md:block sm:hidden xs:hidden hidden">
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 Title
@@ -23,28 +23,22 @@ const MyLibraryListView: FC<MyLibraryGridViewProps> = ({ games }) => {
                             <th scope="col" className="px-6 py-3">
                                 Date Added
                             </th>
-                            <th scope="col" className="px-6 py-3">
-                                Your Rating
-                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {games.map((ug, key) => (
+                        {games.map((g, key) => (
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={key}>
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <img src={ug.platformGame.game.coverUrl} width={70} className="hover:scale-105" />
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white  xl:block lg:block md:block sm:hidden xs:hidden hidden">
+                                    <img src={g.platformGame.game.coverUrl} width={70} className="hover:scale-105" />
                                 </th>
                                 <td className="px-6 py-4">
-                                    {ug.platformGame.game.title}
+                                    {g.platformGame.game.title}
                                 </td>
                                 <td className="px-6 py-4">
-                                    {ug.platformGame.game.publishDate}
+                                    {g.platformGame.game.publishDate}
                                 </td>
                                 <td className="px-6 py-4">
-                                    {/* {ug.datAdded} */}
-                                </td>
-                                <td className="px-6 py-4">
-                                    10/10
+                                    {String(g.datAdded)} 
                                 </td>
                             </tr>
                         ))}
