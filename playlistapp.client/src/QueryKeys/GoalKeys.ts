7c7205ee54d7d71d1a5a42bb5960354b9fd8be51
ParkerS00 <parker.swenson1@students.snow.edu>
2024-11-05 19:12:16 -0700

    Ability to delete a goal in the front end

--- playlistapp.client/src/QueryKeys/GoalKeys.ts
+++ playlistapp.client/src/QueryKeys/GoalKeys.ts
@@ -4,6 +4,7 @@ const GoalKeys = {
   GetGoalsByUser: (userId: string) =>
     ["Goal", "GetGoalsByUser", userId] as const,
   UpdateGoal: ["Goal", "UpdateGoal"],
+  DeleteGoal: (goalId: number) => ["Goal", "DeleteGoal", goalId] as const,
 };
 
 export default GoalKeys;
--- playlistapp.client/src/hooks/GoalQueries.ts
+++ playlistapp.client/src/hooks/GoalQueries.ts
@@ -13,18 +13,54 @@ export const GoalQueries = {
         queryClient.invalidateQueries({ queryKey: keys.AddGoal });
       },
     });
+    /*
+    const addGoalRequest: AddGoalRequest = {
+      achievementId: achievementId,
+      dateToAchieve: date,
+      isCurrent: bool,
+      userId: usr?.guid ?? "",
+    };
+
+    const {
+      data: newGoal,
+      mutate: AddNewGoal,
+      isPending: isAdding,
+      isError: isAddingError,
+      isSuccess: isAddingSuccess,
+    } = GoalQueries.useAddGoal(addGoalRequest);
+
+    const handleAddGoal = () => {
+      AddNewGoal();
+    };
+    */
   },
   useGetGoalById: (goalId: number) => {
     return useQuery({
       queryFn: () => GoalService.getGoalById(goalId),
       queryKey: keys.GetGoalById(goalId),
     });
+    /*
+    const {
+      data: goalById,
+      isPending: isGettingGoalById,
+      isError: isGettingGoalByIdError,
+      isSuccess: isGettingGoalByIdSuccess,
+    } = GoalQueries.useGetGoalById(Number(goalId));
+    */
   },
   useGetGoalsByUser: (userId: string) => {
     return useQuery({
       queryFn: () => GoalService.getGoalsFromUser(userId),
       queryKey: keys.GetGoalsByUser(userId),
     });
+    /*
+    const {
+      data: goalsByUser,
+      isPending: isGettingGoalsByUser,
+      isError: isGettingGoalsByUserError,
+      isSuccess: isGettingGoalsByUserSuccess,
+    } = GoalQueries.useGetGoalsByUser(userId);
+    */
   },
   useUpdateGoal: (updateGoalRequest: UpdateGoalRequest) => {
     const queryClient = useQueryClient();
@@ -34,5 +70,47 @@ export const GoalQueries = {
         queryClient.invalidateQueries({ queryKey: keys.UpdateGoal });
       },
     });
+    /*
+    const updateGoalRequest: UpdateGoalRequest = {
+      dateToAchieve: date,
+      id: Number(goalId),
+      isComplete: bool,
+      isCurrent: bool,
+    };
+
+    const {
+      data: updatedGoal,
+      mutate: UpdateGoal,
+      isPending: isUpdating,
+      isError: isUpdatingError,
+      isSuccess: isUpdatingSuccess,
+    } = GoalQueries.useUpdateGoal(updateGoalRequest);
+
+    const handleUpdateGoal = () => {
+      UpdateGoal();
+    };
+    */
+  },
+  useDeleteGoal: (goalId: number) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: () => GoalService.deleteGoal(goalId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: keys.DeleteGoal(goalId) });
+      },
+    });
+    /*
+    const {
+      data: deletedGoal,
+      mutate: DeleteGoal,
+      isPending: isDeleting,
+      isError: isDeletingError,
+      isSuccess: isDeletingSuccess,
+    } = GoalQueries.useDeleteGoal(Number(goalId));
+
+    const handleDeleteGoal = () => {
+      DeleteGoal();
+    };
+    */
   },
 };
