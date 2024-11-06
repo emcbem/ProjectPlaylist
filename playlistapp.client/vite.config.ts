c9fe79582c3ea8c694a767975dd087c21f720576
ethan.enger <engeethan@gmail.com>
2024-11-05 19:12:46 -0700

    Sorry princess

--- playlistapp.client/vite.config.ts
+++ playlistapp.client/vite.config.ts
@@ -38,11 +38,11 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
   }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT
-  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
-  : env.ASPNETCORE_URLS
-  ? env.ASPNETCORE_URLS.split(";")[0]
-  : "https://localhost:7041";
+// const target = env.ASPNETCORE_HTTPS_PORT
+//   ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+//   : env.ASPNETCORE_URLS
+//   ? env.ASPNETCORE_URLS.split(";")[0]
+//   : "https://localhost:7041";
 
 // Load environment variables
 dotenv.config();
