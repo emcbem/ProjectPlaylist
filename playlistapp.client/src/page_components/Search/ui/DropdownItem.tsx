fa2cd6f5956f0a1750167696469182c8eaf82a60
ethan.enger <engeethan@gmail.com>
2024-11-19 16:35:33 -0700

    Search page is DONE

--- playlistapp.client/src/page_components/Search/ui/DropdownItem.tsx
+++ playlistapp.client/src/page_components/Search/ui/DropdownItem.tsx
@@ -3,9 +3,9 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 export const DropdownItem = ({value, onClick}: {value: string, onClick: () => void}) => {
   return (
     <div
-      className={`rounded-full truncate p-1 px-3 border-2  border-[#111111] dark:border-[#ffffff] m-1 `}
+      className={`rounded-full truncate min-h-[35px] p-1 px-3 border border-[#111111] dark:border-white m-1 `}
     >
-      <div className="flex flex-row">
+      <div className="flex flex-row ">
         <p className="w-4/5 truncate ">
           <span className="transition-opacity">
             {value}
