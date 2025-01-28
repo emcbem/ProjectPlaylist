14492664d323fb23846d8106044a8b794c877ccb
ParkerS00 <parkerswenson00@gmail.com>
2025-01-27 17:52:02 -0700

    Getting access token for playstation complete in the backend

---
+++ PlaylistApp.Server/Controllers/PlaystationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlaylistApp.Server.Services.PlaystationServices;
+
+namespace PlaylistApp.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlaystationController : Controller
+{
+    private readonly PlaystationAuthenticationService playstationService;
+    private readonly IConfiguration config;
+
+    public PlaystationController(PlaystationAuthenticationService playstationAuthenticationService, IConfiguration configuration)
+    {
+        playstationService = playstationAuthenticationService;
+        config = configuration;
+    }
+
+    [HttpPost("gettoken")]
+    public async Task<IActionResult> GetPlaystationAuthenticationToken()
+    {
+        var npsso = config["npsso"];
+
+        if (string.IsNullOrWhiteSpace(npsso))
+        {
+            return BadRequest(new { Error = "npsso token is required." });
+        }
+
+        try
+        {
+            await playstationService.GetPlaystationAuthenticationToken(npsso);
+            return Ok(new { Message = "Token successfully retrieved." });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new {Error = ex.Message});
+        }
+    }
+}
---
+++ PlaylistApp.Server/DTOs/PlaystationData/PlaystationContext.cs
@@ -0,0 +1,13 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+
+namespace PlaylistApp.Server.DTOs.PlaystationData;
+
+public class PlaystationContext
+{
+    public string? AccessToken { get; set; }
+    public string? TokenType { get; set; }
+    public int ExpiresIn { get; set; }
+    public string? Scope { get; set; }
+    public string? IdToken { get; set; }
+    public string? RefreshToken { get; set; }   
+}
