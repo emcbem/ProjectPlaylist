e8cd2f7eeccaf28cb894661221fe315c66b07f9e
ParkerS00 <parkerswenson00@gmail.com>
2025-02-03 20:20:42 -0700

    builds again

--- PlaylistApp.Server/Services/SteamServices/ISteamService.cs
+++ PlaylistApp.Server/Services/SteamServices/ISteamService.cs
@@ -7,4 +7,6 @@ namespace PlaylistApp.Server.Services.SteamServices;
 public interface ISteamService
 {
     public Task<List<SteamActionItem>> GetGamesFromUserBasedOffOfSteamId(string steamId);
+    public string ExtractSteamIdFromUrl(string urlParams);
+    public void AddSteamUserPlatform(string userId, string steamId);
 }
