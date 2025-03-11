0ba99fc08fb433345c373544e6027a72d3942c98
dustyshaw <shustydaw@gmail.com>
2025-03-10 19:06:17 -0600

    SteamAction controller returns a list

---
+++ PlaylistApp.Test/Services/SteamServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using PlaylistApp.Server.Services.SteamServices.SteamAchievementService.SteamAchievementService;
+using PlaylistApp.Server.Services.SteamServices.SteamGameService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistApp.Test.Services;
+
+public class SteamServiceTests
+{
+    [Fact]
+    public void SteamAchievementServiceTest1()
+    {
+        ISteamAchievementService steamAchSvc;
+    }
+}
