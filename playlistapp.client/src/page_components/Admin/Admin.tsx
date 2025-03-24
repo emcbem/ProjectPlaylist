e3559fc77209f89310e93520dd5fcf4f141914b5
emcbem <engerethan@gmail.com>
2025-03-24 16:50:55 -0600

    More admin changes

--- playlistapp.client/src/page_components/Admin/Admin.tsx
+++ playlistapp.client/src/page_components/Admin/Admin.tsx
@@ -1,39 +1,22 @@
+import { RoleRequired } from "../AuthenticationLockers/RoleRequired";
 import IGBDUpdate from "./Components/IGDBUpdate.tsx/IGBDUpdate";
 import PSNUpdate from "./Components/PSNUpdate";
 
 const AdminPage = () => {
-  //   const { usr, isLoading } = useContext(
-  //     UserAccountContext
-  //   ) as UserAccountContextInterface;
-
-  //   if (isLoading) {
-  //     return (
-  //       <div className="flex w-full dark:text-white text-black justify-center sm:mt-28 mt-14">
-  //         Loading...
-  //       </div>
-  //     );
-  //   }
-
-  //   if (!usr || (!usr && !isLoading)) {
-  //     return (
-  //       <div className="flex w-full dark:text-white text-black justify-center sm:mt-28 mt-14">
-  //         Error Fetching Notifications
-  //       </div>
-  //     );
-  //   }
-
   return (
-    <div className="flex w-full justify-center">
-      <div className="flex flex-col w-full max-w-[1264px] dark:text-white text-black justify-start items-start mt-14">
-        <div className="w-full flex lg:justify-start justify-center">
-          <h1 className="text-4xl font-bold ">Admin Page</h1>
-        </div>
-        <div className="w-full my-3 flex lg:flex-row flex-col">
-          <IGBDUpdate />
-          <PSNUpdate />
+    <RoleRequired roleToLookOutFor={"Admin"}>
+      <div className="flex w-full justify-center">
+        <div className="flex flex-col w-full max-w-[1264px] dark:text-white text-black justify-start items-start mt-14">
+          <div className="w-full flex lg:justify-start justify-center">
+            <h1 className="text-4xl font-bold ">Admin Page</h1>
+          </div>
+          <div className="w-full my-3 flex lg:flex-row flex-col">
+            <IGBDUpdate />
+            <PSNUpdate />
+          </div>
         </div>
       </div>
-    </div>
+    </RoleRequired>
   );
 };
 
