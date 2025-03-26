7a01051f0db9070447652ca19b7cb4b9f21d9fe6
ParkerS00 <parkerswenson00@gmail.com>
2025-03-25 18:01:31 -0600

    Database changes done in the backend

---
+++ PlaylistApp.Server/Data/UserGameAuditLog.cs
@@ -0,0 +1,13 @@
+﻿namespace PlaylistApp.Server.Data;
+
+public class UserGameAuditLog
+{
+    public int Id { get; set; }
+    public int PlatformGameId { get; set; }
+    public int UserId { get; set; }
+    public DateTime AuditDate { get; set; }
+    public int MinutesBefore { get; set; }
+    public int MinutesAfter { get; set; }
+    public virtual UserAccount UserAccount { get; set; } = null!;
+    public virtual PlatformGame PlatformGame { get; set; } = null!;
+}
