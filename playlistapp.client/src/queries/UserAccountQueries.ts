854f3036c65e74d1f05a596653762d356c8e6577
emcbem <engerethan@gmail.com>
2025-03-25 21:02:54 -0600

    oops

--- playlistapp.client/src/queries/UserAccountQueries.ts
+++ playlistapp.client/src/queries/UserAccountQueries.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import keys from "@/QueryKeys/UserAccountKeys";
 import { UserAccountService } from "@/ApiServices/UserAccountService";
-import { AddUserRequest } from "@/@types/Requests/AddRequests/addUserRequest";
 import { UpdateUserRequest } from "@/@types/Requests/UpdateRequests/updateUserRequest";
 import toast from "react-hot-toast";
 
