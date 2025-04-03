91a06837167795623064de74ffef30dac7a1b603
dustyshaw <shustydaw@gmail.com>
2025-04-02 19:54:31 -0600

    added recharts for line graph

--- playlistapp.client/src/page_components/WrapUps/WrapUpPage.tsx
+++ playlistapp.client/src/page_components/WrapUps/WrapUpPage.tsx
@@ -8,6 +8,7 @@ import GameCarousel from "./GameCarousel";
 import { WrapUp } from "@/@types/WrapUps/WrapUp";
 import HourBarChart from "./HourBarChart";
 import DateRangeSelector from "./DateRangeSelector/DateRangeSelector";
+import HourLineGraph from "./HourLineGraph";
 
 const WrapUpPage = () => {
   const { userGuid, isLoading: isAccountLoading } = React.useContext(
@@ -59,10 +60,10 @@ const WrapUpPage = () => {
   return (
     <>
       <div className="min-h-screen bg-white dark:bg-black dark:text-white">
-        <div className="grid justify-items-center ">
-          <p>
+        <div className="grid justify-items-center mb-48">
+          {/* <p>
             month: {selectedMonth} - year: {selectedYear}
-          </p>
+          </p> */}
           <div style={{ maxWidth: "1200px" }} className="w-full mt-8">
             <div className="text-center">
               <h1 className="text-3xl my-5">Your Wrap Ups</h1>
@@ -72,23 +73,25 @@ const WrapUpPage = () => {
               setSelectedMonth={handleMonthChange}
             />
           </div>
-          {wrapUp && (
-            <>
-              <div className="text-center mt-24 sm:mt-10 text-2xl dark:text-gray-200">
-                You played{" "}
-                <span className="font-semibold text-4xl dark:text-white">
-                  {
-                    wrapUp?.barGraphGameData.filter((x) => x.timePlayed > 0)
-                      .length
-                  }
-                </span>
-                <br /> games
-              </div>
-              <GameCarousel carouselGames={wrapUp?.gamesPlayed} />
+          {/* {wrapUp && ( */}
+          <>
+            <div className="text-center mt-24 text-2xl dark:text-gray-200">
+              You played{" "}
+              <span className="font-semibold text-4xl dark:text-white">
+                {
+                  wrapUp?.barGraphGameData.filter((x) => x.timePlayed > 0)
+                    .length
+                }
+              </span>
+              <br /> games
+            </div>
+            <GameCarousel carouselGames={wrapUp?.gamesPlayed} />
 
-              <HourBarChart HourBarChartData={wrapUp?.barGraphGameData} />
-            </>
-          )}
+            <HourBarChart HourBarChartData={wrapUp?.barGraphGameData} />
+
+            <HourLineGraph />
+          </>
+          {/* )} */}
         </div>
       </div>
     </>
