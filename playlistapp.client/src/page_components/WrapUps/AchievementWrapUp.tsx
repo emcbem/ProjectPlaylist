37b45ea8d2d410f36d5c8a3314f4a887bdc7c0fe
dustyshaw <shustydaw@gmail.com>
2025-04-09 16:49:51 -0600

    Fixed pipeline - sorry!

--- playlistapp.client/src/page_components/WrapUps/AchievementWrapUp.tsx
+++ playlistapp.client/src/page_components/WrapUps/AchievementWrapUp.tsx
@@ -1,7 +1,6 @@
 import { AchievementGroup } from "@/@types/WrapUps/AchievementGroup";
 import React, { FC } from "react";
-import AchievementCard from "../Achievements/Components/AchievementCard";
-import { AchievementQueries } from "@/queries/AchievementQueries";
+import WrapUpAchievementCard from "./WrapUpAchievementCard";
 
 interface AchievementWrapUpProps {
   achievementGroups: AchievementGroup[];
@@ -11,7 +10,6 @@ const AchievementWrapUp: FC<AchievementWrapUpProps> = ({
   achievementGroups,
 }) => {
   const [expanded, setExpanded] = React.useState<string | false>("panel1");
-
 
   return (
     <div className="mt-8 md:w-1/2 w:4/5">
@@ -60,11 +58,7 @@ const AchievementWrapUp: FC<AchievementWrapUpProps> = ({
               >
                 <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                   {x.achievements.map((y, idx) => (
-                    <AchievementCard
-                      key={idx}
-                      achievement={y}
-                      showAddButton={false}
-                    />
+                    <WrapUpAchievementCard key={idx} achievement={y} />
                   ))}
                 </div>
               </div>
@@ -77,111 +71,3 @@ const AchievementWrapUp: FC<AchievementWrapUpProps> = ({
 };
 
 export default AchievementWrapUp;
-
-// import { AchievementGroup } from "@/@types/WrapUps/AchievementGroup";
-// import React, { FC } from "react";
-// import AchievementCard from "../Achievements/Components/AchievementCard";
-
-// interface AchievementWrapUpProps {
-//   achievementGroups: AchievementGroup[];
-// }
-
-// const AchievementWrapUp: FC<AchievementWrapUpProps> = ({
-//   achievementGroups,
-// }) => {
-//   const [expanded, setExpanded] = React.useState<string | false>("panel1");
-
-//   return (
-//     <div className="mt-8 md:w-1/2 w:4/5">
-//       <div id="accordion-collapse" data-accordion="collapse">
-//         {achievementGroups.map((x, key) => {
-//           return (
-//             <>
-//               <h2 id="accordion-collapse-heading-1" key={key}>
-//                 <button
-//                   type="button"
-//                   className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-//                   data-accordion-target="#accordion-collapse-body-1"
-//                   aria-expanded={`${expanded === `panel${x.gameName}`}`}
-//                   aria-controls="accordion-collapse-body-1"
-//                   onClick={() => setExpanded(`panel1${x.gameName}`)}
-//                 >
-//                   <span>{x.gameName}</span>
-//                   <svg
-//                     data-accordion-icon
-//                     className="w-3 h-3 rotate-180 shrink-0"
-//                     aria-hidden="true"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                     fill="none"
-//                     viewBox="0 0 10 6"
-//                   >
-//                     <path
-//                       stroke="currentColor"
-//                       stroke-linecap="round"
-//                       stroke-linejoin="round"
-//                       stroke-width="2"
-//                       d="M9 5 5 1 1 5"
-//                     />
-//                   </svg>
-//                 </button>
-//               </h2>
-//               <div
-//                 id="accordion-collapse-body-1"
-//                 className={expanded === `panel1${x.gameName}` ? "" : "hidden"}
-//                 aria-labelledby="accordion-collapse-heading-1"
-//               >
-//                 <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-//                   {x.achievements.map((y, key) => {
-//                     <div className="flex items-center space-x-4 rtl:space-x-reverse">
-//                       <div className="flex-shrink-0">
-//                         <img
-//                           className="sm:w-12 sm:h-12 w-8 h-8 rounded-full"
-//                           src={achievement.Achievement.imageURL}
-//                           alt="Achievement Logo"
-//                         />
-//                       </div>
-//                       <div className="flex-1 min-w-0">
-//                         <p className="md:text-2xl sm:text-xl text-tiny font-medium text-gray-900  dark:text-white">
-//                           {achievement.Achievement.name}{" "}
-//                           {achievement?.Earned?.isSelfSubmitted && (
-//                             <span className="text-sm font-sans font-light text-gray-400">
-//                               Self Submitted
-//                             </span>
-//                           )}
-//                           <br />
-//                           {achievement?.Earned?.dateAchieved && (
-//                             <span className="text-sm font-sans font-light text-gray-400">
-//                               {formatDate(achievement?.Earned?.dateAchieved)}
-//                             </span>
-//                           )}
-//                         </p>
-//                         <p className="md:text-lg sm:text-base text-tiny text-gray-500 dark:text-gray-400">
-//                           {usr?.guid && !showAddButton && (
-//                             <p>{achievement.Achievement?.description}</p>
-//                           )}
-//                         </p>
-//                       </div>
-//                       <div className="inline-flex items-center md:text-lg sm:text-base text-sm font-semibold text-gray-900 dark:text-white">
-//                         <div className="relative inline-block md:ml-4 ml-0 cursor-pointer">
-//                           {usr?.guid && showAddButton && (
-//                             <AchievementModalParent
-//                               achievement={achievement.Achievement}
-//                               earned={achievement.Earned!}
-//                               userGuid={usr.guid}
-//                             />
-//                           )}
-//                         </div>
-//                       </div>
-//                     </div>;
-//                   })}
-//                 </div>
-//               </div>
-//             </>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AchievementWrapUp;
